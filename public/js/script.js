4fe60d7a023e4fa19fca8820db32db5594c96934
Nojyto <nojusskirkevicius@gmail.com>
2021-09-10 23:36:21 +0300

    1.1

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,8 @@
-const timeOptions = {hour: "numeric", minute: "numeric", second:"numeric", hour12: false}
-const text = document.getElementById("text")
-const time = document.getElementById("time")
-const timetable = [
+const timeOptions = {hour: "numeric", minute: "numeric", second: "numeric", hour12: false}
+const text        = document.getElementById("text")
+const curTime     = document.getElementById("curTime")
+const curSec      = document.getElementById("curSec")
+const timetable   = [
     {s : "07:10:00", e : "07:55:00", i : 0},
     {s : "08:00:00", e : "08:45:00", i : 1},
     {s : "08:55:00", e : "09:40:00", i : 2},
@@ -27,21 +28,36 @@ function SecToHMS(s) {
 }
 
 
-function updateTime(d){
-    if(timetable[8].e < d){
-        text.innerHTML = "Šulė baigės"
-        time.innerHTML = ""
+function updateTime(d, wd){
+    console.log(d, wd)
+
+    if((timetable[8].e < d && wd == 5) || wd == 6 || wd == 7){
+        text.innerHTML    = "Savaitgalis"
+        curTime.innerHTML = ""
+        curSec.innerHTML  = ""
+    }else if(timetable[8].e < d){
+        text.innerHTML    = "Šulė baigės"
+        curTime.innerHTML = ""
+        curSec.innerHTML  = ""
     }else{
         for(const curEv of timetable){
             if(d < curEv.s){
-                text.innerHTML = `${curEv.i} pamoka prasideda už`
-                time.innerHTML = `${SecToHMS(HMSToSec(curEv.s) - HMSToSec(d))}`
+                let curDate       = (SecToHMS(HMSToSec(curEv.s) - HMSToSec(d))).split(":")
+                text.innerHTML    = `${curEv.i} pamoka prasideda už`
+                curTime.innerHTML = `${curDate[0]}∶${curDate[1]}∶`
+                curSec.innerHTML  = `${curDate[2]}`
                 break
             }
             if(d < curEv.e){
-                text.innerHTML = `${curEv.i} pamoka baigias už`
-                time.innerHTML = `${SecToHMS(HMSToSec(curEv.e) - HMSToSec(d))}`
+                let curDate       = (SecToHMS(HMSToSec(curEv.e) - HMSToSec(d))).split(":")
+                text.innerHTML    = `${curEv.i} pamoka baigias už`
+                curTime.innerHTML = `${curDate[0]}∶${curDate[1]}∶`
+                curSec.innerHTML  = `${curDate[2]}`
                 break
+                /*if(HMSToSec(curEv.e) - HMSToSec(d) == 5 * 60){
+                    const endOfReminder = new Notification("5 min ligi pamokos pabaigos", {icon: "dependencies/clock.png"})
+                    greeting.onclick = () => window.open(window.location.href)
+                }*/
             }
         }
     }
@@ -50,16 +66,18 @@ function updateTime(d){
 /*window.onload = () => {
     //test
     const d = new Date()
-    updateTime(new Date(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours() - 13, d.getMinutes() +32, d.getSeconds()).toLocaleTimeString("en-US", timeOptions))
+    updateTime(new Date(d.getFullYear(), d.getMonth(), 4, d.getHours() - 13, d.getMinutes() +32, d.getSeconds()).toLocaleTimeString("en-US", timeOptions), new Date().getDay() - 3)
     window.setInterval(() => {
         const d = new Date()
-        updateTime(new Date(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours() - 13, d.getMinutes()+32, d.getSeconds()).toLocaleTimeString("en-US", timeOptions))
+        updateTime(new Date(d.getFullYear(), d.getMonth(), 4, d.getHours() - 13, d.getMinutes() + 32, d.getSeconds()).toLocaleTimeString("en-US", timeOptions), new Date().getDay() - 3)
     }, 1000)
 }*/
 
-window.onload = () => {
-    updateTime(new Date().toLocaleTimeString("en-US", timeOptions))
+window.onload = async () => {
+    //await Notification.requestPermission()
+    updateTime(new Date().toLocaleTimeString("en-US", timeOptions), new Date().getDay())
     window.setInterval(() => {
-        updateTime(new Date().toLocaleTimeString("en-US", timeOptions))
+        updateTime(new Date().toLocaleTimeString("en-US", timeOptions), new Date().getDay())
     }, 1000)
 }
+
