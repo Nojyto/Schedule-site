bb79d589b216b38aa54c0ba96b8b0a233c4d3593
Nojyto <nojusskirkevicius@gmail.com>
2021-09-12 11:24:23 +0300

    1.3

--- public/js/script.js
+++ public/js/script.js
@@ -27,11 +27,9 @@ function SecToHMS(s) {
     return sign + z(s/3600 |0) + ':' + z((s%3600) / 60 |0) + ':' + z(s%60)
 }
 
-
 function updateTime(d, wd){
-    console.log(d, wd)
-
-    if((timetable[8].e < d && wd == 5) || wd == 6 || wd == 7){
+    //console.log(d, wd)
+    if((timetable[8].e < d && wd == 5) || wd == 6 || wd == 0){
         text.innerHTML    = "Savaitgalis"
         curTime.innerHTML = ""
         curSec.innerHTML  = ""
@@ -66,16 +64,18 @@ function updateTime(d, wd){
 /*window.onload = () => {
     //test
     const d = new Date()
-    updateTime(new Date(d.getFullYear(), d.getMonth(), 4, d.getHours() - 13, d.getMinutes() +32, d.getSeconds()).toLocaleTimeString("en-US", timeOptions), new Date().getDay() - 3)
+    updateTime(new Date(d.getFullYear(), d.getMonth(), d.getDay() - 3, d.getHours(), d.getMinutes() +32, d.getSeconds()).toLocaleTimeString("en-US", timeOptions), 3)
+    ScrollReveal().reveal(".hero", {distance: '60px', duration: 1500, delay: 150,})
     window.setInterval(() => {
         const d = new Date()
-        updateTime(new Date(d.getFullYear(), d.getMonth(), 4, d.getHours() - 13, d.getMinutes() + 32, d.getSeconds()).toLocaleTimeString("en-US", timeOptions), new Date().getDay() - 3)
+        updateTime(new Date(d.getFullYear(), d.getMonth(), d.getDay() - 3, d.getHours(), d.getMinutes() + 32, d.getSeconds()).toLocaleTimeString("en-US", timeOptions), 3)
     }, 1000)
 }*/
 
 window.onload = async () => {
     //await Notification.requestPermission()
     updateTime(new Date().toLocaleTimeString("en-US", timeOptions), new Date().getDay())
+    ScrollReveal().reveal(".hero", {distance: '60px', duration: 1500, delay: 150,})
     window.setInterval(() => {
         updateTime(new Date().toLocaleTimeString("en-US", timeOptions), new Date().getDay())
     }, 1000)
